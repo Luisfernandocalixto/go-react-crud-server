a847bfb9be399ad3d307f19d07d1afd4f641e951
“Calixto” <l19070031@huetamo.tecnm.mx>
2026-03-10 17:30:43 -0600

    Initialized repository

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	app.Static("/", "./public")
+
+	app.Get("/users", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{
+			"data": "Usuarios desde el backend",
+		})
+	})
+
+	app.Listen(":" + port)
+	fmt.Println("Server on port " + port)
+}
